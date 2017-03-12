69c62cce65a1440068a1a04900ee44542eddd154
Alexandre Teixeira <ateixeira0163@gmail.com>
2017-03-12 17:48:41 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
+#include "gameboard.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    GameBoard gameBoard;
 
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("gameBoard", &gameBoard);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
--- main.qml
+++ main.qml
@@ -3,14 +3,13 @@ import QtQuick.Window 2.2
 
 Window {
     visible: true
-    width: 360
-    height: 360
+    width: 300
+    height: 300
     title: qsTr("2048")
 
     MainForm {
         anchors.fill: parent
-            id: gameWindow
+        id: gameWindow
 
-            }
-
-}
+        }
+    }

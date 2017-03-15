c213e31a86b0a348c4e27762b89ceaa805ee2af7
Alexandre Teixeira <ateixeira0163@gmail.com>
2017-03-15 18:24:51 +0100

    Commentaires ajoutés à tile.h

--- tile.h
+++ tile.h
@@ -8,30 +8,30 @@
 class Tile
 {
 public:
-    Tile(int x=10, int y=10);
+    Tile(int x=10, int y=10);           // Constructeur de la classe Tile
 
-    void setPosition(int x=0, int y=0);
-    void setColor();
-    void setFusion(bool f = false);
-    void multNumber(int n=2);
-    void setNumber(int a=2);
-    void resetTile();
+    void setPosition(int x=0, int y=0); // Si x ou y = 0 on change seulement l'autre
+    void setColor();                    // Mettre les couleurs selon le nombre
+    void setFusion(bool f = false);     // Pour vérifier si le Tile a déjà été fusionné
+    void multNumber(int n=2);           // Pour multiplier le Tile par 2
+    void setNumber(int a=2);            // Pour initialiser le Tile
+    void resetTile();                   // Pour mettre le Tile à 0
 
-    int getPosition(int a);
-    QString getColor();
-    QString getTextColor();
-    QString getNumber();
-    bool getFusion();
-    int *getRefPosVect();
-    QString *getRefColor();
-    int *getRefNumber();
+    int getPosition(int a);             // Recupère la position
+    QString getColor();                 // Recupère la couleur
+    QString getTextColor();             // Recupère la couleur du text
+    QString getNumber();                // Recupère le nombre
+    bool getFusion();                   // Recupère l'état de fusion
+    int *getRefPosVect();               // Recupère la référence de la position
+    QString *getRefColor();             // Recupère la référence de la couleur
+    int *getRefNumber();                // Recupère la référence du nombre
 
 
 protected:
-    int tilePosition[2];
-    QString tileColor, textColor;
-    int number;
-    bool fusion;
+    int tilePosition[2];                // Position x,y du Tile
+    QString tileColor, textColor;       // Couleur du Tile et du text
+    int number;                         // Nombre du Tile
+    bool fusion;                        // État de fusion du Tile
 };
 
 #endif // TILE_H

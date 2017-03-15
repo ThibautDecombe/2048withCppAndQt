38a191ed6cccb6ea70d21e31ccbf043e3f86b223
Alexandre Teixeira <ateixeira0163@gmail.com>
2017-03-15 18:26:54 +0100

    Commentaires ajoutés à main.cpp

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
-    GameBoard gameBoard;
+    QGuiApplication app(argc, argv);    // On crée l'application QGUI
+    GameBoard gameBoard;                // On crée l'objet de la classe GameBoard
 
     QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty("gameBoard", &gameBoard);
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.rootContext()->setContextProperty("gameBoard", &gameBoard); // On peut appeler gameBoard
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));                // désormais dans le fichier qml
 
-    return app.exec();
+    return app.exec();  // On exécute l'application
 }
